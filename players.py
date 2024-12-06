abb6570a1fb1d119e40ea59b5fb6813c58c08d03
Katie Scheer <katiescheer@mac.mynetworksettings.com>
2024-12-06 11:18:00 -0500

    Tracks suggestions and deductions

--- players.py
+++ players.py
@@ -2,10 +2,13 @@ class Player:
     def __init__(self, name, starting_room):
         self.name = name
         self.current_room = starting_room
+        self.notebook = []  
 
-    def move(self, new_room, mansion):
-        if new_room in mansion.rooms[self.current_room]:
-            self.current_room = new_room
-            print(f"{self.name} moved to the {new_room}.")
-        else:
-            print(f"Invalid move! {new_room} is not connected to {self.current_room}.")
+    def add_to_notebook(self, entry):
+        self.notebook.append(entry)
+        print(f"Added to notebook: {entry}")
+
+    def show_notebook(self):
+        print(f"\n--- {self.name}'s Notebook ---")
+        for entry in self.notebook:
+            print(entry)
